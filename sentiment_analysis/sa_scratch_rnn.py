6588d49f738dc70e24b0bc3d8ee875ee8b77c8ef
HoangPham3003 <hoang.ph303@gmail.com>
2024-10-29 15:48:36 +0000

    update scratch rnn

--- sentiment_analysis/sa_scratch_rnn.py
+++ sentiment_analysis/sa_scratch_rnn.py
@@ -9,11 +9,6 @@ import tensorflow as tf
 
 from utils.dataset_utils import get_traintest_tweet
 
-def tanh_func(x):
-    return np.maximum(x, 0)
-
-def sigmoid_func(x):
-    return 1 / (1 + np.exp(-x))
 
 class RNN_cell(tf.keras.layers.Layer):
     def __init__(self, input_dim, output_dim):
@@ -21,38 +16,62 @@ class RNN_cell(tf.keras.layers.Layer):
         input params:
             - input_dim: embedding dim of each word (shape [1, embedding_dim])
             - output_dim: length of output h_t and y
-            - activation: tanh or sigmoid
         output:
             - y: predicted value
             - h_t: current state
         """
         super(RNN_cell, self).__init__()
         
-        self.w_hx = np.random.standard_normal((input_dim, output_dim))
-        self.w_hh = np.random.standard_normal((output_dim, output_dim))
-        self.b_h = np.random.standard_normal((1, output_dim))
+        self.tanh = tf.keras.activations.tanh
+        self.sigmoid = tf.keras.activations.sigmoid
         
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         
-        self.activation = None
+        self.w_hx = self.add_weight(
+            shape=(self.input_dim, self.output_dim),
+            initializer="random_normal",
+            trainable=True,
+            name="Whx",
+        )
         
-        self.tanh = tanh_func
-        self.sigmoid = sigmoid_func
+        self.w_hh = self.add_weight(
+            shape=(self.output_dim, self.output_dim),
+            initializer="random_normal",
+            trainable=True,
+            name="Whh",
+        )
         
-        if activation == 'tanh':
-            self.activation = tf.keras.activations.tanh()
-        elif activation == 'sigmoid':
-            self.activation = tf.keras.activations.sigmoid()
-            
-    """ 25/10/2024: Update y 
-    """
+        self.b_h = self.add_weight(
+            shape=(1, self.output_dim),
+            initializer="zeros",
+            trainable=True,
+            name="b_h",
+        )
+        
+        self.w_hy = self.add_weight(
+            shape=(self.output_dim, 1),
+            initializer="random_normal",
+            trainable=True,
+            name="w_hy",
+        )
+        
+        self.b_y = self.add_weight(
+            shape=(1, 1),
+            initializer="zeros",
+            trainable=True,
+            name="b_y",
+        )
+        
     def call(self, x, h_0):
-        h_t = np.dot(h_0, self.w_hh) + np.dot(x, self.w_hx) + self.b_h
+        h_t = tf.matmul(h_0, self.w_hh) + tf.matmul(x, self.w_hx) + self.b_h
         h_t = self.tanh(h_t)
-        y = self.sigmoid(h_t)
+        y = tf.matmul(h_t, self.w_hy) + self.b_y
+        y = self.sigmoid(y)
         return y, h_t
 
 
-class ScratchRNN(tf.keras.Model):
+class ScratchRNNLayer(tf.keras.Model):
     def __init__(self, num_words, embedding_dim=64, max_len=51, hidden_dim=128):
         """ Define RNN model architecture
         Args:
@@ -63,30 +82,33 @@ class ScratchRNN(tf.keras.Model):
         Returns:
             - RNN model
         """
-        super(ScratchRNN, self).__init__()
+        super(ScratchRNNLayer, self).__init__()
         
         self.n_cells = max_len
         self.hidden_dim = hidden_dim
         
+        self.input_layer = tf.keraslayers.Input(shape=(None, 32, 32))
+        
         self.embedding = tf.keras.layers.Embedding(num_words, embedding_dim, input_length=max_len)
         
         self.rnn_blocks = []
-        for _ in self.n_cells:
-            rnn_cell = RNN_cell(input_dim=embedding_dim, output_dim=hidden_dim, activation='tanh')
+        for _ in range(self.n_cells):
+            rnn_cell = RNN_cell(input_dim=embedding_dim, output_dim=hidden_dim)
             self.rnn_blocks.append(rnn_cell)
         self.rnn_blocks.append(rnn_cell)
         
         
     def call(self, x_padded):
+        x_padded = self.input_layer(x_padded)
         x = self.embedding(x_padded)
         
-        h_0 = None
-        for i in len(self.RNN_block):
-            rnn_cell = RNN_block[i]
-            x_t = x[i]
-            if i == 0:
-                h_0 = np.zeros((1, self.hidden_dim))
-            y, h_0 = rnn_cell(x_t, h_0)
+        batch_size = tf.shape(x)[0]
+        h_t = tf.zeros((batch_size, self.hidden_dim))
+        
+        for i in range(self.n_cells):
+            rnn_cell = self.rnn_blocks[i]
+            x_t = x[:, i, :]
+            y, h_t = rnn_cell(x_t, h_t)
         
         return y
     
@@ -94,4 +116,9 @@ class ScratchRNN(tf.keras.Model):
 def sa_scratch_rnn_running():
     # train_x_padded, val_x_padded, train_y, val_y = get_traintest_tweet()
     
-    n_cells = train_x_padded[0].shape()[0]
+    # n_cells = train_x_padded[0].shape()[0]
+    
+    model = ScratchRNNLayer(num_words=1000)
+    model.build(input_shape=(None, 32, 32))
+    print(model.summary())
+    
