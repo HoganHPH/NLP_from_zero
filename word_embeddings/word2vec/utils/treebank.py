396c769770c645efd4b395060b0f042143a87352
root <HoangPham3003>
2024-12-06 10:53:08 +0000

    update work embeddings

--- word_embeddings/word2vec/utils/treebank.py
+++ word_embeddings/word2vec/utils/treebank.py
@@ -10,7 +10,7 @@ class StanfordTreebank:
             
         self.path = path
         self.tablesize = tablesize
-        
+
     def tokenize(self):
         if hasattr(self, "_tokens") and self._tokens:
             return self._tokens
@@ -21,7 +21,7 @@ class StanfordTreebank:
         revtokens = []
         idx = 0
         
-        for sentence in self.getSentences():
+        for sentence in self._sentences:
             for w in sentence:
                 wordcount += 1
                 if not w in tokens:
@@ -43,11 +43,11 @@ class StanfordTreebank:
         self._revtokens = revtokens
         return self._tokens
     
-    def getSentences(self):
+    def getRawSentences(self):
         if hasattr(self, "_sentences") and self._sentences:
             return self._sentences
         
-        sentences = []
+        rawSentences = []
         
         with open(os.path.join(self.path, "datasetSentences.txt"), 'r') as f:
             first = True
@@ -57,44 +57,42 @@ class StanfordTreebank:
                     continue
 
                 splitted = line.strip().split()[1:]
-                sentences += [[w.lower() for w in splitted]]
+                rawSentences += [[w.lower() for w in splitted]]
 
-        self._sentences = sentences
-        self._sentlengths = np.array([len(s) for s in sentences])
+        self._rawSentences = rawSentences
+        self._sentlengths = np.array([len(s) for s in rawSentences])
         self._cumsentelen = np.cumsum(self._sentlengths)
           
-        return self._sentences
+        return self._rawSentences
     
     def getNumSentences(self):
         if hasattr(self, "_numSentences") and self._numSentences:
             return self._numSentences
         else:
-            self._numSentences = len(self.sentences)
+            self._numSentences = len(self._rawSentences)
             return self._numSentences
             
-    def getAllSentences(self):
-        if hasattr(self, "_allsentences") and self._allsentences:
-            return self._allsentences
+    def getAugmentedSentences(self):
+        if hasattr(self, "_augmentedSentences") and self._augmentedSentences:
+            return self._augmentedSentences
         
-        sentences = self.getSentences()
+        rawSentences = self.getRawSentences()
         tokens = self.tokenize()
         rejectProb = self.rejectProb()
-        allSentences = [[w for w in s 
+        augmentedSentences = [[w for w in s 
             if 0 >= rejectProb[tokens[w]] or random.random() >= rejectProb[tokens[w]]
             ]
-            for s in sentences * 30
+            for s in rawSentences * 30
         ]
              
-        allSentences = [s for s in allSentences if len(s) > 1]
-        
-        self._allSentences = allSentences
-        
-        return self._allSentences
+        augmentedSentences = [s for s in augmentedSentences if len(s) > 1]
+        self._augmentedSentences = augmentedSentences
+        return self._augmentedSentences
     
     def getRamdomContext(self, C=5):
-        allSent = self.getAllSentences()
-        sentID = random.randint(0, len(a) - 1)
-        sent = allSent[sentID]
+        augmentedSenteces = self._augmentedSentences
+        sentID = random.randint(0, len(augmentedSenteces) - 1)
+        sent = augmentedSenteces[sentID]
         wordID = random.randint(0, len(sent) - 1)
         
         context = sent[max(0, wordID - C):wordID] # before context
@@ -110,33 +108,43 @@ class StanfordTreebank:
             return self.getRandomContext(C)
 
     
-    def sent_labels(self):
+    def getSentLabels(self):
         if hasattr(self, "_sent_labels") and self._sent_labels:
             return self._sent_labels
-        
+
         dictionary = dict()
         phrases = 0
-        
-        with open(os.path.join(self.path,"/dictionary.txt"), 'r') as f:
-            furst = True
+        with open(self.path + "/dictionary.txt", "r") as f:
+            for line in f:
+                line = line.strip()
+                if not line: continue
+                splitted = line.split("|")
+                dictionary[splitted[0].lower()] = int(splitted[1])
+                phrases += 1
+
+        labels = [0.0] * phrases
+        with open(self.path + "/sentiment_labels.txt", "r") as f:
+            first = True
             for line in f:
                 if first:
                     first = False
                     continue
-                
+
                 line = line.strip()
-                if not line: 
-                    continue
-                spllited = line.split("|")
-                labels[int(spliiitedp0)] = float(spllited[1])
-                
-        senf_labels = [0.0] * self.numSentences
-        sentences = self.getSentences()
-        for i in xrange(self.getNumSentences()):
+                if not line: continue
+                splitted = line.split("|")
+                labels[int(splitted[0])] = float(splitted[1])
+
+        sent_labels = [0.0] * self.getNumSentences()
+        sentences = self._rawSentences
+        for i in range(self.getNumSentences()):
             sentence = sentences[i]
-            full_sent = " ".join(sentence).replace('-lrb-', ('(').replace('-rrb-', ')'))
+            full_sent = " ".join(sentence).replace('-lrb-', '(').replace('-rrb-', ')')
+            if full_sent not in dictionary:
+                print(f"Sentence not found in dictionary: {full_sent}")
+                continue
             sent_labels[i] = labels[dictionary[full_sent]]
-            
+
         self._sent_labels = sent_labels
         return self._sent_labels
      
@@ -144,8 +152,8 @@ class StanfordTreebank:
         if hasattr(self, "_split") and self._split:
             return self._split
         
-        split = [[] for i in xrange(3)]
-        with open(os.path.join(self.path, "/datasetSplit.txt"), 'r') as f:
+        split = [[] for i in range(3)]
+        with open(os.path.join(self.path, "../DATA/datasetSplit.txt"), 'r') as f:
             first = True
             for line in f:
                 if first:
@@ -159,7 +167,7 @@ class StanfordTreebank:
     def getRandomTrainSentence(self):
         split = self.dataset_split()
         sentDb = split[0][random.randint(0, len(split[0])-1)]
-        return self.sentences()[sentId], self.categorify(self.sent_labels()[sentId])
+        return self.getRawSentences()[sentId], self.categorify(self.sent_labels()[sentId])
     
     def categorify(self, label):
         if label <= 0.2:
@@ -192,9 +200,9 @@ class StanfordTreebank:
         
         nTokens = len(self.tokenize())
         samplingFreq = np.zeros((nTokens,))
-        self.getAllSentences()
+        self.getAugmentedSentences()
         i = 0
-        for w in xrange(nTokens):
+        for w in range(nTokens):
             w = self._revtokens[i]
             if w in self._tokensfreq:
                 freq = 1.0 * self._tokensfreq[w]
@@ -209,7 +217,7 @@ class StanfordTreebank:
         self._sampleTable = [0] * self.tablesize
         
         j = 0
-        for i in xrange(self.tablesize):
+        for i in range(self.tablesize):
             while i > samplingFreq[j]:
                 j += 1
             self._sampleTable[i] = j
@@ -222,12 +230,12 @@ class StanfordTreebank:
         
         threshold = 1e-5 * self._wordcount
         
-        nTokens = len(self.tokenize())
+        nTokens = len(self._tokens)
         rejectProb = np.zeros((nTokens, ))
         for i in range(nTokens):
             w = self._revtokens[i]
-            freq = 1.0* self._tokensfreq[w]
-            rejectProb[i] = max(0, 1, np.sqrt(threshold / freq))
+            freq = 1.0 * self._tokensfreq[w]
+            rejectProb[i] = max(0, 1 - np.sqrt(threshold / freq))
             
         self._rejectProb = rejectProb
         return self._rejectProb
